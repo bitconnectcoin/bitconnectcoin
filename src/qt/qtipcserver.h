0634dbff36acbca93dfb5d456bab8c24d5dae8c3
bitconnectcoin <bitconnect@tutanota.com>
2016-12-31 12:44:18 +0530

    Bitconnect Coin Project

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define bitconnect-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "bitconnectURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
